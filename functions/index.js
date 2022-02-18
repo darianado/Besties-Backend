ea0368021253041c4ba6717bc479e59741eb63bd
Nikolaj B. Jensen <nikko4913@icloud.com>
2022-02-18 10:17:29 +0000

    Add deleteHash function to remove a hash when user entry is removed

--- functions/index.js
+++ functions/index.js
@@ -9,10 +9,18 @@ exports.getRecHTTPs = functions.https.onRequest((req, res) => {
     return res.send({ "status": 200, "message": "Ready to handle recommendation request.", "userId": userId })
 })
 
-// Listen for changes in all documents in the 'users' collection
+// Listen for creations in all documents in the 'users' collection
 exports.createHash = functions.firestore.document('users/{userId}').onCreate((snapshot, context) => {
       const data = snapshot.data();
       const h = hashing.hash(data.interests);
 
       return admin.firestore().collection(`user_hashes/${h}/users`).doc(context.params.userId).set({});
+});
+
+// Listen for deletions in all documents in the 'users' collection
+exports.deleteHash = functions.firestore.document('users/{userId}').onDelete((snapshot, context) => {
+  const data = snapshot.data();
+  const h = hashing.hash(data.interests);
+
+  return admin.firestore().collection(`user_hashes/${h}/users`).doc(context.params.userId).delete();
 });
